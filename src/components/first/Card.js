839752acc290b377a525d082cfe82fc2b0add271
inyoung <rmdnps10@gmail.com>
2023-08-11 19:31:26 +0900

    페이지 폴더 안에 컴포넌트 추가

--- src/components/first/Card.js
+++ src/components/first/Card.js
@@ -1,30 +1,23 @@
 import React from "react";
 import { styled } from "styled-components";
+import CardDirection from "./CardDirection";
 function Card() {
   return (
-    <Container>
+    <CardContainer>
       <div className="title">
         <img src="images/호선/2호선.png" alt="2호선" className="train-number" />
         <span className="number">2호선</span>
       </div>
       <div className="direction_container">
-        <Direction>
-          <div className="inner">
-            <div className="direction_type">내선순환</div>
-          </div>
-        </Direction>
-        <Direction>
-          <div className="inner">
-            <p className="direction_type">내선순환</p>
-          </div>
-        </Direction>
+        <CardDirection />
+        <CardDirection />
       </div>
-    </Container>
+    </CardContainer>
   );
 }
-const Container = styled.div`
+const CardContainer = styled.div`
   width: 82vw;
-  margin: 17px auto 0;
+  margin: 17px auto 12px;
   .title {
     font-size: 26px;
     display: flex;
@@ -48,28 +41,6 @@ const Container = styled.div`
     display: flex;
     justify-content: space-between;
     margin-top: 14px;
-  }
-`;
-
-const Direction = styled.div`
-  background: linear-gradient(
-    180deg,
-    rgba(86, 188, 50, 0.8) 0%,
-    rgba(86, 188, 50, 0.496) 100%
-  );
-  border-radius: 20px;
-  width: 40vw;
-  box-shadow: -2px 4px 4px rgba(0, 0, 0, 0.25);
-  height: 138px;
-  .inner {
-    margin: 0 auto;
-    width: 35vw;
-    margin-top: 52px;
-  }
-  .direction_type {
-    margin-left: 10px;
-    font-size: 20px;
-    font-weight: 500;
   }
 `;
 
---
+++ src/components/first/CardList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+import { styled } from "styled-components";
+
+function CardList() {
+  //  배열 받아와서 반복 횟수만큼 Card 컴포넌트 생성
+  // 생성되는 Card엔  {direction (어디 행), {역1,역2,역3,역4,역5} (인접한 역 5개) } 전달해줘야함
+  return (
+    <CardListContainer>
+      <Card />
+    </CardListContainer>
+  );
+}
+
+const CardListContainer = styled.div`
+  margin: 0 20px;
+`;
+
+export default CardList;
