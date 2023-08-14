2140980710c30d032f4da252c31fe5ee17498c3a
inyoung <rmdnps10@gmail.com>
2023-08-15 01:35:05 +0900

    객체 요청했다는 가정하에 첫번째 페이지 구성

--- src/components/first/CardDirection.js
+++ src/components/first/CardDirection.js
@@ -1,32 +1,184 @@
 import React from "react";
 import styled from "styled-components";
 
-function CardDirection() {
+function CardDirection({ stationNum, stationList, subwayList }) {
+  const rightMovingList = [5, 37, 69];
+  const leftMovingList = [10, 38, 72];
+  const rightStaticList = [-5, 20, 53, 78];
+  const leftStaticList = [-5, 23, 53, 82];
+  const rightDirectionList = subwayList.filter(
+    (subway) => subway.way === "right" && subway.state === "진행중"
+  );
+  const rightStaticDirectionList = subwayList.filter(
+    (subway) => subway.way === "right" && subway.state === "정차"
+  );
+  const leftDirectionList = subwayList.filter(
+    (subway) => subway.way === "left" && subway.state === "진행중"
+  );
+
+  const leftStaticDirectionList = subwayList.filter(
+    (subway) => subway.way === "left" && subway.state === "정차"
+  );
+
+  // const dummyList = [
+  //   {
+  //     id: "0",
+  //     stationNum: "5",
+  //     stationList: ["신길역", "영등포시장", "영등포구청", "앙평역"],
+  //     subwayList: [
+  //       {
+  //         id: 3028,
+  //         way: "right",
+  //         type: "일반",
+  //         state: "진행중",
+  //         before: "신길",
+  //         after: "영등포시장",
+  //       },
+  //       {
+  //         id: 1023,
+  //         way: "left",
+  //         type: "일반",
+  //         state: "정차",
+  //         before: "양평",
+  //         after: "영등포구청",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: "1",
+  //     stationNum: "2",
+  //     stationList: ["문래역", "영등포구청", "당산", "합정"],
+  //     subwayList: [
+  //       { id: 100, way: "right", type: "일반", state: "진행중",before: "합정", after: "당산" },
+  //       { id: 200, way: "left", type: "일반", state: "정차",before: "영등포구청", after: "당산" },
+  //     ],
+  //   },
+  // ];
+
   return (
     <DirectionContainer>
       <div className="container">
         <div className="upper-subway">
-          <img src="/images/지하철-오른쪽방향.png" alt="오른쪽 지하철" />
+          {rightDirectionList.map((subway, index) => (
+            <img
+              key={subway.id}
+              src={
+                subway.type === "일반"
+                  ? "images/지하철-오른쪽방향.png"
+                  : "images/지하철(급행)-오른쪽.png"
+              }
+              alt="오른쪽 지하철"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: `${
+                  subway.before === stationList[0]
+                    ? rightMovingList[0]
+                    : subway.before === stationList[1]
+                    ? rightMovingList[1]
+                    : subway.before === stationList[2]
+                    ? rightMovingList[2]
+                    : ""
+                }%`,
+              }}
+            />
+          ))}
+          {rightStaticDirectionList.map((subway, index) => (
+            <img
+              key={subway.id}
+              src={
+                subway.type === "일반"
+                  ? "images/지하철-오른쪽방향.png"
+                  : "images/지하철(급행)-오른쪽.png"
+              }
+              alt="오른쪽 지하철"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: `${
+                  subway.before === stationList[0]
+                    ? rightStaticList[0]
+                    : subway.before === stationList[1]
+                    ? rightStaticList[1]
+                    : subway.before === stationList[2]
+                    ? rightStaticList[2]
+                    : subway.before === stationList[3]
+                    ? rightStaticList[3]
+                    : ""
+                }%`,
+              }}
+            />
+          ))}
         </div>
         <div className="station_list">
-          {/* 호선마다 색깔 달라져야되고, 역명도 props으로 전달되서 있어야함 */}
-          <div className="line">
-            <div className="circle">
-              <li>신길</li>
-            </div>
-            <div className="circle">
-              <li>영등포시장</li>
-            </div>
-            <div className="circle">
-              <li>영등포구청</li>
-            </div>
-            <div className="circle">
-              <li>양평</li>
-            </div>
+          <div
+            className="line"
+            style={{ backgroundColor: `var(--${stationNum}호선)` }}
+          >
+            {stationList.map((station, index) => {
+              return (
+                <div
+                  className="circle"
+                  style={{ border: `2px solid var(--${stationNum}호선)` }}
+                  key={index}
+                >
+                  <li>{station}</li>
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="lower-subway">
-          <img src="/images/지하철-왼쪽방향.png" alt="왼쪽 지하철" />
+          {leftDirectionList.map((subway, index) => (
+            <img
+              key={subway.id}
+              src={
+                subway.type === "일반"
+                  ? "images/지하철-왼쪽방향.png"
+                  : "images/지하철(급행)-왼쪽.png"
+              }
+              alt="왼쪽 지하철"
+              style={{
+                position: "absolute",
+                top: 0,
+                right: `${
+                  subway.before === stationList[3]
+                    ? leftMovingList[0]
+                    : subway.before === stationList[2]
+                    ? leftMovingList[1]
+                    : subway.before === stationList[1]
+                    ? leftMovingList[2]
+                    : ""
+                }%`,
+              }}
+            />
+          ))}
+          {leftStaticDirectionList.map((subway, index) => (
+            <img
+              key={subway.id}
+              src={
+                subway.type === "일반"
+                  ? "images/지하철-왼쪽방향.png"
+                  : "images/지하철(급행)-왼쪽.png"
+              }
+              alt="왼쪽 지하철"
+              style={{
+                position: "absolute",
+                top: 0,
+                right: `${
+                  subway.before === stationList[3]
+                    ? leftStaticList[0]
+                    : subway.before === stationList[2]
+                    ? leftStaticList[1]
+                    : subway.before === stationList[1]
+                    ? leftStaticList[2]
+                    : subway.before === stationList[0]
+                    ? leftStaticList[3]
+                    : ""
+                }%`,
+              }}
+            />
+          ))}
         </div>
       </div>
     </DirectionContainer>
@@ -37,7 +189,7 @@ const DirectionContainer = styled.div`
   background-color: #160c23;
   border-radius: 15px;
   width: 100%;
-  height: 150px;
+  height: 135px;
   .container {
     margin: 0 auto;
   }
@@ -52,16 +204,21 @@ const DirectionContainer = styled.div`
     width: 58px;
     position: absolute;
     top: 0;
-    left: 150px;
+    left: 0;
   }
 
   .lower-subway {
     width: 86%;
+    position: relative;
     display: flex;
+    height: 28px;
     margin: 30px auto 0;
   }
   .lower-subway img {
     width: 58px;
+    position: absolute;
+    top: 0;
+    right: 0;
   }
 
   .station_list {
@@ -71,12 +228,13 @@ const DirectionContainer = styled.div`
   }
 
   .station_list .line {
-    width: 96%;
-    height: 2px;
+    width: 92%;
+    height: 3px;
     background-color: var(--5호선);
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin: 0 auto;
   }
 
   .station_list .line .circle {
@@ -90,10 +248,10 @@ const DirectionContainer = styled.div`
   .station_list .line .circle li {
     position: absolute;
     width: 60px;
-    font-size: 10px;
+    font-size: 13px;
     text-align: center;
     height: 20px;
-    top: 12px;
+    top: 15px;
     left: -30px;
   }
 `;
--- src/components/first/CardList.js
+++ src/components/first/CardList.js
@@ -6,10 +6,59 @@ function CardList() {
   //  배열 받아와서 반복 횟수만큼 Card 컴포넌트 생성
   // 생성되는 Card엔  {호선, {역1,역2,역3,역4}, 지하철의 상대적 위치 } 전달해줘야함
   // 예를 들어서
-  // {{5, {신길, 영등포시장, 영등포구청, 양평}, 지하철1{신길->영등포시장}, 지하철2{양평->영등포구청}}},{2, {문래, 영등포구청, 당산, 합정}, 지하철1{문래->영등포구청}, 지하철2{당산역 정차중}} };
+  const dummyList = [
+    {
+      id: "0",
+      stationNum: "5",
+      stationList: ["신길", "영등포시장", "영등포구청", "양평"],
+      subwayList: [
+        {
+          id: 3028,
+          way: "right",
+          type: "일반",
+          state: "진행중",
+          before: "영등포구청", 
+          after: "양평",
+        },
+        {
+          id: 1023,
+          way: "left",
+          type: "일반",
+          state: "정차",
+          before: "신길",
+          after: "",
+        },
+      ],
+    },
+    {
+      id: "1",
+      stationNum: "2",
+      stationList: ["문래역", "영등포구청", "당산", "합정"],
+      subwayList: [
+        {
+          id: 100,
+          way: "right",
+          type: "일반",
+          state: "진행중",
+          before: "당산",
+          after: "합정",
+        },
+        {
+          id: 200,
+          way: "left",
+          type: "일반",
+          state: "진행중",
+          before: "합정",
+          after: "당산",
+        },
+      ],
+    },
+  ];
   return (
     <CardListContainer>
-      <Card />
+      {dummyList.map((card, index) => {
+        return <Card {...card} key={index} />;
+      })}
     </CardListContainer>
   );
 }
